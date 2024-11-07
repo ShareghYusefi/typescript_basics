4588f24fdb44bd96ff87d2d2c45d02e9f892bdbb
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-06 17:58:36 -0700

    data types

--- app.js
+++ app.js
@@ -27,3 +27,43 @@ console.log(y); // 10
 var z = 10;
 // z = 20; // error
 // "Let" is preferred over "var" as best practice.
+// Data types in TypeScript
+// We can define a variables data type using a colon.
+// Built-in data types
+// number, string, boolean, void, null, undefined
+var customMessage = "Hello, World!";
+var num = 10;
+var isValid = true;
+var nullValue = null;
+var undefinedValue = undefined;
+function greet() {
+    console.log("Hello, World!");
+    // does not return anything, so return type is void
+    return;
+}
+// Any data type
+// any
+var anyValue = 10;
+anyValue = "Hello, World!";
+// Type operators
+console.log(typeof num); // number
+console.log(typeof customMessage); // string
+console.log(typeof isValid); // boolean
+// User-defined data types
+// Arrays, tuples, classes and interfaces
+// Arrays
+var numbers = [1, 2, 3, 4, 5];
+var names = ["John", "Doe", "Jane"];
+// Tuples: when we have a known number of elements with different data types.
+var person = ["John", 25, true];
+// Classes
+var Person = /** @class */ (function () {
+    function Person(nameArg, ageArg, isStudentArg) {
+        this.name = nameArg;
+        this.age = ageArg;
+        this.isStudent = isStudentArg;
+    }
+    return Person;
+}());
+var person1 = new Person("John", 25, true);
+console.log(person1);
--- app.ts
+++ app.ts
@@ -36,3 +36,55 @@ const z = 10;
 // z = 20; // error
 
 // "Let" is preferred over "var" as best practice.
+
+// Data types in TypeScript
+// We can define a variables data type using a colon.
+
+// Built-in data types
+// number, string, boolean, void, null, undefined
+let customMessage: string = "Hello, World!";
+let num: number = 10;
+let isValid: boolean = true;
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+
+function greet(): void {
+  console.log("Hello, World!");
+  // does not return anything, so return type is void
+  return;
+}
+
+// Any data type
+// any
+let anyValue: any = 10;
+anyValue = "Hello, World!";
+
+// Type operators
+console.log(typeof num); // number
+console.log(typeof customMessage); // string
+console.log(typeof isValid); // boolean
+
+// User-defined data types
+// Arrays, tuples, classes and interfaces
+// Arrays
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ["John", "Doe", "Jane"];
+
+// Tuples: when we have a known number of elements with different data types.
+let person: [string, number, boolean] = ["John", 25, true];
+
+// Classes
+class Person {
+  name: string;
+  age: number;
+  isStudent: boolean;
+
+  constructor(nameArg: string, ageArg: number, isStudentArg: boolean) {
+    this.name = nameArg;
+    this.age = ageArg;
+    this.isStudent = isStudentArg;
+  }
+}
+
+let person1 = new Person("John", 25, true);
+console.log(person1);
